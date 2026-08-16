276fcbbc7031e437799bf47f4f2dbfb6e24cb83e
Rojit <138610570+Rojit-Timilsena@users.noreply.github.com>
2026-08-16 21:39:38 +0545

    Version Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.resona.music"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
